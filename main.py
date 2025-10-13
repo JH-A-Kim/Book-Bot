4f2c5fdd17e4f847a8f525240c2b545c3095adb5
jak28 <jak28@sfu.ca>
2025-10-12 22:54:49 -0700

    updated reporting and display functions

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from stats import get_book_text, num_words, unique_characters
+from stats import get_book_text, num_words, unique_characters, sorted_list
 
 def main():
     file_contents=get_book_text("books/frankenstein.txt")
     words = num_words("books/frankenstein.txt")
     unique_chars = unique_characters("books/frankenstein.txt")
+    unique_chars = sorted_list(unique_chars)
+
 
     print(f"Found {words} total words")
-    print(unique_chars)
+    for char, count in unique_chars:
+        print(f"{char}: {count}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -14,13 +14,18 @@ def unique_characters(String):
     with open(String) as f:
         file_contents = f.read()
     lowered=file_contents.lower()
+    stripped=lowered.replace(" ", "")
     
     unique_characters={
 
     }
-    for char in lowered:
+    for char in stripped:
         if unique_characters.__contains__(char):
             unique_characters[char]+=1
         else:
             unique_characters[char]=1
     return unique_characters
+
+def sorted_list(Dict):
+    sorted_dict=sorted(Dict.items(), key=lambda item: item[1], reverse=True)
+    return sorted_dict
